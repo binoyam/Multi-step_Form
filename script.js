cb238b1839034d2952de7e4013c81e4230c22349
binoyam <binyam.techan01@gmail.com>
2024-01-19 01:49:46 +0300

    javascript

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const steps = document.querySelectorAll(".step_container");
-const circleSteps = document.querySelectorAll("span .step_circle");
+const stepOneForm = document.getElementById("step_one_form");
+const stepOneNextBtn = document.getElementById("step_one_next")
+stepOneForm.addEventListener("submit", verifyStepOne)
+stepOneNextBtn.addEventListener("click", verifyStepOne)
+
+const verifyStepOne = (e) => {
+    e.preventDefault();
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone_number");
+}
