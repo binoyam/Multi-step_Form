65011892ebb803b3937764f721ca8e03e2e5f121
binoyam <binyam.techan01@gmail.com>
2024-01-20 03:33:33 +0300

    form verification

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 const stepOneForm = document.getElementById("step_one_form");
 const stepOneNextBtn = document.getElementById("step_one_next");
-const nameError = document.querySelector(".name_error");
-const emailError = document.querySelector(".email_error");
-const phoneError = document.querySelector(".phone_error");
-
+const stepOne = document.querySelector(".step_1");
+const stepTwo = document.querySelector(".step_2");
 stepOneForm.addEventListener("submit", stepOneCheck);
 
 function stepOneCheck(e) {
   e.preventDefault();
-  var nameInput = document.getElementById("name");
-  var emailInput = document.getElementById("email");
-  var phoneInput = document.getElementById("phone_number");
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const phoneInput = document.getElementById("phone_number");
+
+  const nameError = document.querySelector(".name_error");
+  const emailError = document.querySelector(".email_error");
+  const phoneError = document.querySelector(".phone_error");
 
   if (nameInput.value.trim() === "") {
     nameError.style.display = "block";
-    nameInput.classList.add("error_border")
+    nameInput.classList.add("error_border");
   } else {
     nameError.style.display = "none";
-    nameInput.classList.remove("error_border")
+    nameInput.classList.remove("error_border");
   }
   if (emailInput.value.trim() === "") {
     emailError.style.display = "block";
-    emailInput.classList.add("error_border")
-    
+    emailInput.classList.add("error_border");
   } else {
     emailError.style.display = "none";
-    emailInput.classList.remove("error_border")
+    emailInput.classList.remove("error_border");
   }
   if (phoneInput.value.trim() === "") {
     phoneError.style.display = "block";
-    phoneInput.classList.add("error_border")
-    
+    phoneInput.classList.add("error_border");
   } else {
     phoneError.style.display = "none";
-    phoneInput.classList.remove("error_border")
+    phoneInput.classList.remove("error_border");
+  }
+  if (
+    nameInput.value.trim() != "" &&
+    emailInput.value.trim() != "" &&
+    phoneInput.value.trim() != ""
+  ) {
+    stepOne.style.display = "none";
+    stepTwo.style.display = "block";
+  } else {
+    stepOne.style.display = "block";
+    stepTwo.style.display = "none";
   }
 }
-/* im stuck */
