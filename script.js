dc36b2d835b93dbb121dc703a7735f38757e71e7
binoyam <binyam.techan01@gmail.com>
2024-01-22 03:32:34 +0300

    starting over

--- script.js
+++ script.js
@@ -3,27 +3,51 @@ const step_one_nextBtn = document.getElementById("next_btn_1");
 const step_two_nextBtn = document.getElementById("next_btn_2");
 const step_three_nextBtn = document.getElementById("next_btn_3");
 const step_four_nextBtn = document.getElementById("next_btn_4");
-const completedForm = document.getElementById("thankyou")
 
-
+const step_two_backBtn = document.getElementById("back_btn_2");
+const step_three_backBtn = document.getElementById("back_btn_3");
+const step_four_backBtn = document.getElementById("back_btn_4");
+const completedForm = document.getElementById("thankyou");
 
 let currentStep = 1;
-function showStep(stepNumber){
+function showStep(stepNumber) {
   allSteps.forEach((step) => {
-    step.style.display = 'none'
-  })
-  document.getElementById(`step_${stepNumber}`).style.display = 'block';
-}
-function nextStep(){
-  currentStep++;
-  if(currentStep === 4){
-    completedForm.style.display = 'block'
+    step.style.display = "none";
+  });
+  if (stepNumber == 5) {
+    document.getElementById("step_4").style.display = "none";
+    completedForm.style.display = "block";
   }
-  showStep(currentStep);
-
+  document.getElementById(`step_${stepNumber}`).style.display = "block";
 }
-showStep(currentStep)
-step_one_nextBtn.addEventListener("click", nextStep );
-step_two_nextBtn.addEventListener("click", nextStep);
-step_three_nextBtn.addEventListener("click",nextStep );
-step_four_nextBtn.addEventListener("click", nextStep);
+function nextStep(stepNumber) {
+  currentStep = stepNumber;
+  showStep(currentStep);
+}
+function previousStep(stepNumber) {
+  currentStep = stepNumber;
+  showStep(currentStep);
+}
+
+step_one_nextBtn.addEventListener("click", () => {
+  nextStep(2);
+});
+step_two_nextBtn.addEventListener("click", () => {
+  nextStep(3);
+});
+step_three_nextBtn.addEventListener("click", () => {
+  nextStep(4);
+});
+step_four_nextBtn.addEventListener("click", () => {
+  nextStep(5);
+});
+
+step_two_backBtn.addEventListener("click", () => {
+  previousStep(1);
+});
+step_three_backBtn.addEventListener("click", () => {
+  previousStep(2);
+});
+step_four_backBtn.addEventListener("click", () => {
+  previousStep(3);
+});
