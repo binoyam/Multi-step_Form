a854a579f3a1588c6b8746e3566b6ee9334d5d72
binoyam <binyam.techan01@gmail.com>
2024-01-23 03:18:44 +0300

    veryfy form 1

--- script.js
+++ script.js
@@ -9,7 +9,57 @@ const step_three_backBtn = document.getElementById("back_btn_3");
 const step_four_backBtn = document.getElementById("back_btn_4");
 const completedForm = document.getElementById("thankyou");
 
+const stepCircle = document.querySelectorAll(".step_circle");
+const stepOneForm = document.getElementById("step_one_form");
 let currentStep = 1;
+
+function stepOneCheck(e) {
+  e.preventDefault();
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const phoneInput = document.getElementById("phone_number");
+
+  const nameError = document.querySelector(".name_error");
+  const emailError = document.querySelector(".email_error");
+  const phoneError = document.querySelector(".phone_error");
+
+  if (nameInput.value.trim() === "") {
+    nameError.style.display = "block";
+    nameInput.classList.add("error_border");
+  } else {
+    nameError.style.display = "none";
+    nameInput.classList.remove("error_border");
+  }
+  if (emailInput.value.trim() === "") {
+    emailError.style.display = "block";
+    emailInput.classList.add("error_border");
+  } else {
+    emailError.style.display = "none";
+    emailInput.classList.remove("error_border");
+  }
+  if (phoneInput.value.trim() === "") {
+    phoneError.style.display = "block";
+    phoneInput.classList.add("error_border");
+  } else {
+    phoneError.style.display = "none";
+    phoneInput.classList.remove("error_border");
+  }
+  if (
+    nameInput.value.trim() != "" &&
+    emailInput.value.trim() != "" &&
+    phoneInput.value.trim() != ""
+  ) {
+    currentStep++;
+
+    stepCircle.forEach((circle) => {
+      circle.classList.remove("active");
+    });
+    stepCircle[currentStep].classList.add("active");
+    nextStep(2);
+  }
+}
+
+
 function showStep(stepNumber) {
   allSteps.forEach((step) => {
     step.style.display = "none";
@@ -30,8 +80,10 @@ function previousStep(stepNumber) {
   showStep(currentStep);
 }
 
-step_one_nextBtn.addEventListener("click", () => {
-  nextStep(2);
+step_one_nextBtn.addEventListener("click", (e) => {
+  stepOneCheck(e)
+    
+  
 });
 step_two_nextBtn.addEventListener("click", () => {
   nextStep(3);
